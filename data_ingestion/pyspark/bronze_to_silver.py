d49860efd3203dc56da0724a649610e4c52d9a67
drjpant <dheerajpant21@gmail.com>
2024-04-07 16:15:27 +0530

    Adding the directory and sub-directory

--- data_ingestion/pyspark/bronze_to_silver.py
+++ data_ingestion/pyspark/bronze_to_silver.py
@@ -14,12 +14,6 @@ if os.environ.get('Foo') is not None:
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-#os.environ["PYSPARK_PYTHON"] = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7"
-#os.environ["PYSPARK_DRIVER_PYTHON"] = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7"
-#os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
-#os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
-#os.environ['SPARK_HOME']= sys.executable
-print('Hello Dheeraj')
 print('spark context set')
 
 
@@ -30,15 +24,15 @@ my_conf.set("spark.master","local[*]")
 
 spark= SparkSession.builder.config(conf= my_conf).getOrCreate()
 
-orderDF= spark.read.option("header",True).option("inferSchema",True).json(r"C:\Users\91812\Downloads\data1.txt")
-orderDF=orderDF.drop("cluster_id","authors_byline",)
-orderDF=orderDF.withColumn("insertdate",current_date()).withColumn("insertdtm",current_timestamp())
+DF= spark.read.option("header",True).option("inferSchema",True).json(r"C:\Users\91812\Downloads\data1.txt")
+DF=DF.drop("cluster_id","authors_byline",)
+DF=DF.withColumn("insertdate",current_date()).withColumn("insertdtm",current_timestamp())
 #orderDF=orderDF.withColumn("insertdtm",current_timestamp())
 #groupdf=orderDF.repartition(4).where("order_customer_id> 10000").select("order_id","order_customer_id") \
 #    .groupby("order_customer_id").count()
-orderDF.show(20)
+DF.show(20)
 
-orderDF.write.parquet(r"C:\Users\91812\Downloads\data1.parquet")
+DF.write.parquet(r"C:\Users\91812\Downloads\data1.parquet")
 #orderDF.printSchema()
 
 #orderDF.show()
